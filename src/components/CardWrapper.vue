1fce1bd3464856a8d08b4bc2e200e2569cf54db1
carlos.melero <cmelero96@gmail.com>
2021-10-15 22:16:53 +0200

    Slotted card for reusability

---
+++ src/components/CardWrapper.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card-wrapper">
+    <slot name="header"></slot>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card-wrapper {
+  border: 3px solid black;
+  border-radius: 1em;
+  width: fit-content;
+  margin: 2em auto;
+}
+</style>
--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="card-wrapper">
-    <header>{{ pokemon.name }}</header>
-    <section><img :src="pokemon.image"></section>
-  </div>
+  <card-wrapper>
+    <template v-slot:header>
+      <header>{{ pokemon.name }}</header>
+    </template>
+    <template v-slot:default>
+      <section><img :src="pokemon.image"></section>
+    </template>
+  </card-wrapper>
 </template>
 
 <script>
+import CardWrapper from './CardWrapper.vue';
+
 export default {
+  components: { CardWrapper },
   props: {
     pokemon: {
       type: Object,
@@ -20,22 +27,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  .card-wrapper {
-    border: 3px solid black;
-    border-radius: 1em;
-    width: fit-content;
-    margin: 2em auto;
+header {
+  font-size: large;
+  font-weight: bolder;
+  border-bottom: 2px solid black;
+  padding: 1em 0 0.5em;
+  margin: -1px;
+}
 
-    header {
-      font-size: large;
-      font-weight: bolder;
-      border-bottom: 2px solid black;
-      padding: 1em 0 0.5em;
-      margin: -1px;
-    }
-
-    section {
-      padding: 1em;
-    }
-  }
+section {
+  padding: 1em;
+}
 </style>
