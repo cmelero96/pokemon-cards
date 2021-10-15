b15e47c241eabd610f82bc278c9499bfb3bd94d5
carlos.melero <cmelero96@gmail.com>
2021-10-15 22:18:45 +0200

    Fix slots template

--- src/components/CardWrapper.vue
+++ src/components/CardWrapper.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="card-wrapper">
+    <header>
     <slot name="header"></slot>
-    <slot></slot>
+    </header>
+    <section>
+      <slot></slot>
+    </section>
   </div>
 </template>
 
@@ -11,5 +15,17 @@
   border-radius: 1em;
   width: fit-content;
   margin: 2em auto;
+
+    header {
+      font-size: large;
+      font-weight: bolder;
+      border-bottom: 2px solid black;
+      padding: 1em 0 0.5em;
+      margin: -1px;
+    }
+
+    section {
+      padding: 1em;
+    }
 }
 </style>
--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,11 +1,7 @@
 <template>
   <card-wrapper>
-    <template v-slot:header>
-      <header>{{ pokemon.name }}</header>
-    </template>
-    <template v-slot:default>
-      <section><img :src="pokemon.image"></section>
-    </template>
+    <template v-slot:header>{{ pokemon.name }}</template>
+    <template v-slot:default><img :src="pokemon.image"></template>
   </card-wrapper>
 </template>
 
@@ -25,17 +21,3 @@ export default {
   },
 };
 </script>
-
-<style scoped lang="scss">
-header {
-  font-size: large;
-  font-weight: bolder;
-  border-bottom: 2px solid black;
-  padding: 1em 0 0.5em;
-  margin: -1px;
-}
-
-section {
-  padding: 1em;
-}
-</style>
